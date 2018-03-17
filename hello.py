010f3ea859cd2729d1c9b88bfa65b3c501deb963
ihysk <sirouma.09@gmail.com>
2018-03-17 07:34:41 +0000

    Added GET /temperature command to get it via bme280

--- hello.py
+++ hello.py
@@ -1,10 +1,23 @@
+import bme280
 import flask
+import smbus2
+
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+
+bme280.load_calibration_params(bus, address)
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World"
+
+@app.route('/temperature')
+def temperature():
+    data = bme280.sample(bus, address)
+    return "{}".format(data.temperature)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
